594b5039b968260811e01386688d280ce4f09199
LorenzoTTGT <lorenzotarghetta@gmail.com>
2021-12-26 17:47:36 +0100

    Update Lorenzo_TT_multiFormatExport.lua

--- LorenzoTT_Scripts/Lorenzo_TT_multiFormatExport.lua
+++ LorenzoTT_Scripts/Lorenzo_TT_multiFormatExport.lua
@@ -2,9 +2,7 @@
 ReaScript name: LorenzoTT_Multi Format Render
 Version: 1.0.4
 Author: LorenzoT 
-@provides 
-../LorenzoTT_Libs/LorenzoTT_GetImageSize.lua 
-../LorenzoTT_libs/LorenzoTT_WRITE_PDF_TO_DISK.lua
+@provides ../LorenzoTT_Libs/LorenzoTT_GetImageSize.lua ../LorenzoTT_libs/LorenzoTT_WRITE_PDF_TO_DISK.lua
 
 ]]
 
