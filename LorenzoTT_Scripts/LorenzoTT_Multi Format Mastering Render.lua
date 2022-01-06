b6f15713d00c939d16e78a37072554ee5f5bbc04
LorenzoTTGT <lorenzotarghetta@gmail.com>
2022-01-06 19:54:54 +0100

    Update LorenzoTT_Multi Format Mastering Render.lua

--- LorenzoTT_Scripts/LorenzoTT_Multi Format Mastering Render.lua
+++ LorenzoTT_Scripts/LorenzoTT_Multi Format Mastering Render.lua
@@ -1,6 +1,6 @@
 --[[
 ReaScript name: LorenzoTT_Multi Format Mastering Render
-Version: 1.0.5
+Version: 1.0.6
 Author: Lorenzo Targhetta
 @provides 
     ../LorenzoTT_Libs/LorenzoTT_GetImageSize.lua 
