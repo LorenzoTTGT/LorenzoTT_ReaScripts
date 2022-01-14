52ae30523e08e0c59ce5485f17960dd24fe34fdc
LorenzoTTGT <95323757+LorenzoTTGT@users.noreply.github.com>
2022-01-14 15:27:18 +0100

    Update LorenzoTT_Add Time Marker from Cursor Region Time Position.lua

--- Mastering Utility/LorenzoTT_Add Time Marker from Cursor Region Time Position.lua
+++ Mastering Utility/LorenzoTT_Add Time Marker from Cursor Region Time Position.lua
@@ -1,18 +1,25 @@
 --[[
 ReaScript name: LorenzoTT_Add Time Marker from Cursor Region Time Position
-Version: 1.0.1
+Version: 1.0.2
 Author: Lorenzo Targhetta
 ]]
 
 function addTimeMarkerFromRegionTimePos()
-	local play_pos = reaper.GetCursorPosition() 
-	local marker_idx, region_idx = reaper.GetLastMarkerAndCurRegion(0, play_pos)
+	play_state = reaper.GetPlayState()
+	if play_state == 0 
+		then 
+		cursPos = reaper.GetCursorPosition() 
+	else 
+		cursPos = reaper.GetPlayPosition()
+	end
+
+	local marker_idx, region_idx = reaper.GetLastMarkerAndCurRegion(0, cursPos)
 	local offset = reaper.GetProjectTimeOffset( proj, false )
 	local _, _, region_start, region_end, region_name, markrgnindexnumber, region_color = reaper.EnumProjectMarkers3(0, region_idx)
-	local cursPosSecs = reaper.GetCursorPosition()
 	local buf = play_pos - region_start - offset
 	local cursPosHHMMSS = reaper.format_timestr(buf,"...")
-	reaper.AddProjectMarker(0,false,buf,0,cursPosHHMMSS,-1)
+
+	reaper.AddProjectMarker(0,false,cursPos,0,cursPosHHMMSS,-1)
 end
 
 addTimeMarkerFromRegionTimePos()
